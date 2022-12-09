0298dcaa4468197118f0da5bd0a79b20b745a2eb
Florimond Manca <florimondmanca@users.noreply.github.com>
2022-12-10 00:12:30 +0100

    n was rows*cols

--- src/day8.rs
+++ src/day8.rs
@@ -3,16 +3,17 @@ use std::collections::HashSet;
 
 pub fn run() {
   let content = include_str!("inputs/8.txt");
-  let heights = parse_heights(content);
-  let count = count_visible(heights);
+  let (heights, n) = parse_heights(content);
+  let count = count_visible(heights, n);
   println!("Answer (part 1): {count}");
 }
 
 type Heights = HashMap<(usize, usize), u32>;
 
-fn parse_heights(content: &str) -> Heights {
+fn parse_heights(content: &str) -> (Heights, usize) {
   let mut heights: Heights = HashMap::new();
-  
+  let n = content.lines().count();
+ 
   for (i, line) in content.lines().enumerate() {
     line
       .chars()
@@ -23,12 +24,11 @@ fn parse_heights(content: &str) -> Heights {
       });
   }
   
-  return heights;
+  return (heights, n);
 }
 
-fn count_visible(heights: Heights) -> u32 {
+fn count_visible(heights: Heights, n: usize) -> u32 {
   let mut visible: HashSet<(usize, usize)> = HashSet::new();
-  let n = heights.len();
   
   for row in 0..n {
     for col in 0..n {
