45a3678a76ee4ad58f9955fe35eb3d08a14f5a9f
florimondmanca <florimond.manca@protonmail.com>
2022-12-11 12:15:03 +0100

    Refactor day 9

--- src/day9.rs
+++ src/day9.rs
@@ -6,23 +6,21 @@ pub fn run() {
     let content = include_str!("inputs/9.txt");
     let moves = parse(content);
 
-    let knots = vec![(0, 0), (0, 0)];
-    println!("Answer (part 1): {}", solve(knots.clone(), &moves));
+    let rope = vec![(0, 0), (0, 0)];
+    println!("Answer (part 1): {}", solve(rope.clone(), &moves));
 
-    let knots = (0..10).map(|_| (0, 0)).collect::<Vec<Knot>>();
-    println!("Answer (part 2): {}", solve(knots.clone(), &moves));
+    let rope = (0..10).map(|_| (0, 0)).collect::<Vec<Knot>>();
+    println!("Answer (part 2): {}", solve(rope.clone(), &moves));
 }
 
-fn solve(knots: Vec<Knot>, moves: &Vec<Move>) -> usize {
-    let mut rope = Rope::new(knots);
+fn solve(mut rope: Rope, moves: &Vec<Move>) -> usize {
+    let mut visited: HashSet<Knot> = HashSet::new();
+    visited.insert(tail(&rope));
 
-    let mut visited: HashSet<(i32, i32)> = HashSet::new();
-    visited.insert(rope.tail());
-
-    for mv in moves {
-        for _ in 0..mv.quantity {
-            rope.move_towards(&mv.direction.as_str());
-            visited.insert(rope.tail());
+    for (direction, quantity) in moves {
+        for _ in 0..*quantity {
+            rope = move_towards(direction.as_str(), &rope);
+            visited.insert(tail(&rope));
         }
     }
 
@@ -30,103 +28,8 @@ fn solve(knots: Vec<Knot>, moves: &Vec<Move>) -> usize {
 }
 
 type Knot = (i32, i32);
-
-fn moved(knot: Knot, displacement: (i32, i32)) -> Knot {
-    (knot.0 + displacement.0, knot.1 + displacement.1)
-}
-
-struct Rope {
-    knots: Vec<Knot>,
-}
-
-impl Rope {
-    fn new(knots: Vec<Knot>) -> Self {
-        Self { knots }
-    }
-
-    fn tail(&self) -> Knot {
-        self.knots.last().unwrap().clone()
-    }
-
-    fn move_towards(&mut self, direction: &str) {
-        let mut new_knots: Vec<Knot> = Vec::new();
-
-        let mut head = self.knots.first().unwrap().clone();
-
-        // The rope head moves according to commands.
-        let new_head = match direction {
-            "U" => moved(head, (0, 1)),
-            "R" => moved(head, (1, 0)),
-            "D" => moved(head, (0, -1)),
-            "L" => moved(head, (-1, 0)),
-            _ => panic!("unknown direction"),
-        };
-        new_knots.push(new_head);
-
-        // Each other knot catches up on the knot before.
-        head = new_head;
-
-        for knot in self.knots.iter().skip(1) {
-            let knot = knot.clone();
-            let new_knot = catch_up(knot, head);
-            new_knots.push(new_knot);
-            head = new_knot;
-        }
-
-        assert!(self.knots.len() == new_knots.len());
-
-        self.knots = new_knots;
-    }
-}
-
-fn catch_up(tail: Knot, head: Knot) -> Knot {
-    let (dx, dy) = (head.0 - tail.0, head.1 - tail.1);
-
-    let displacement = match (dx, dy) {
-        (0, 0) => (0, 0),
-        (0, 1) => (0, 0),
-        (0, 2) => (0, 1),
-        (0, -1) => (0, 0),
-        (0, -2) => (0, -1),
-        (1, 0) => (0, 0),
-        (1, 1) => (0, 0),
-        (1, 2) => (1, 1),
-        (1, -1) => (0, 0),
-        (1, -2) => (1, -1),
-        (2, 0) => (1, 0),
-        (2, 1) => (1, 1),
-        (2, 2) => (1, 1),
-        (2, -1) => (1, -1),
-        (2, -2) => (1, -1),
-        (-1, 0) => (0, 0),
-        (-1, 1) => (0, 0),
-        (-1, 2) => (-1, 1),
-        (-1, -1) => (0, 0),
-        (-1, -2) => (-1, -1),
-        (-2, 0) => (-1, 0),
-        (-2, 1) => (-1, 1),
-        (-2, 2) => (-1, 1),
-        (-2, -1) => (-1, -1),
-        (-2, -2) => (-1, -1),
-        _ => panic!("unknown transition: ({dx}, {dy})"),
-    };
-
-    moved(tail, displacement)
-}
-
-struct Move {
-    direction: String,
-    quantity: u32,
-}
-
-impl Move {
-    fn new(direction: String, quantity: u32) -> Self {
-        Self {
-            direction,
-            quantity,
-        }
-    }
-}
+type Rope = Vec<Knot>;
+type Move = (String, u32);
 
 fn parse(content: &str) -> Vec<Move> {
     content
@@ -134,7 +37,62 @@ fn parse(content: &str) -> Vec<Move> {
         .map(|line| {
             let (direction, quantity) = line.split(" ").collect_tuple().unwrap();
             let quantity = quantity.parse::<u32>().unwrap();
-            Move::new(String::from(direction), quantity)
+            (String::from(direction), quantity)
         })
         .collect()
+}
+
+fn moved(knot: Knot, displacement: (i32, i32)) -> Knot {
+    (knot.0 + displacement.0, knot.1 + displacement.1)
+}
+
+fn tail(rope: &Rope) -> Knot {
+    rope.last().unwrap().clone()
+}
+
+fn move_towards(direction: &str, rope: &Rope) -> Rope {
+    let mut moved_rope: Rope = vec![];
+
+    // The rope head moves according to commands.
+    let mut head = rope.first().unwrap().clone();
+    let moved_head = match direction {
+        "U" => moved(head, (0, 1)),
+        "R" => moved(head, (1, 0)),
+        "D" => moved(head, (0, -1)),
+        "L" => moved(head, (-1, 0)),
+        _ => panic!("unknown direction"),
+    };
+    moved_rope.push(moved_head);
+
+    // Other knots catch up on the knot before them.
+    head = moved_head;
+    for knot in rope.iter().skip(1) {
+        let knot = knot.clone();
+        let moved_knot = catch_up(knot, head);
+        moved_rope.push(moved_knot);
+        head = moved_knot;
+    }
+    assert!(moved_rope.len() == rope.len());
+
+    moved_rope
+}
+
+fn catch_up(tail: Knot, head: Knot) -> Knot {
+    let gap_x = head.0 - tail.0;
+    let gap_y = head.1 - tail.1;
+    let dist = gap_x.abs() + gap_y.abs();
+
+    let dx = if gap_x.abs() <= 1 && dist <= 2 {
+        0
+    } else {
+        gap_x.signum()
+    };
+
+    let dy = if gap_y.abs() <= 1 && dist <= 2 {
+        0
+    } else {
+        gap_y.signum()
+    };
+
+    moved(tail, (dx, dy))
 }
